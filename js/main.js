56d3b13277ca26f956ad446ebaf6c29f8df4db99
BadgerFool <dominicwheeler@outlook.com>
2019-12-29 04:22:55 +0000

    added jQuery for burger menu

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
-    
+
+    //HAMBURGER MENU TOGGLE
+    $('.nav-toggle').click(function() {
+        $('.main-nav').toggleClass('is-open');
+        $('.hamburger').toggleClass('is-open');
+    })
 })
